1d0f511dd9e951b1a0531fde1a922fb2e38cfc83
Ahkar02468 <32251773+Ahkar02468@users.noreply.github.com>
2019-08-23 20:10:37 +0630

    started dot net core project

--- DotNetCorePluralsight/Startup.cs
+++ DotNetCorePluralsight/Startup.cs
@@ -20,10 +20,7 @@ namespace DotNetCorePluralsight
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            
 
             app.Run(async (context) =>
             {
