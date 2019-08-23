afaf0edd5f94c04def1f2979fbb86e1c0b767ef5
Ahkar02468 <32251773+Ahkar02468@users.noreply.github.com>
2019-08-23 23:15:30 +0630

    install jquery

--- DotNetCorePluralsight/wwwroot/js/index.js
+++ DotNetCorePluralsight/wwwroot/js/index.js
@@ -2,3 +2,11 @@
 
 var form = document.getElementById("form");
 form.hidden = true;
+
+var button = document.getElementById("buybutton");
+button.addEventListener("click", function () {
+    alert("Buying Item")
+});
+
+var productinfo = document.getElementsByClassName("properties");
+var ListItems = productinfo.item[0].children;
