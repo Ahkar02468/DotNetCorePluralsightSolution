c5ac597f9dfa65ca4cbdfab0b848d904e20d0776
Ahkar Shwebaw <arkarshwebaw@gmail.com>
2019-08-28 22:32:39 +0630

    Adding mail services

--- DotNetCorePluralsight/Controllers/AppController.cs
+++ DotNetCorePluralsight/Controllers/AppController.cs
@@ -1,6 +1,8 @@
-﻿using DotNetCorePluralsight.ViewModels;
+﻿using DotNetCorePluralsight.Services;
+using DotNetCorePluralsight.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +11,12 @@ namespace DotNetCorePluralsight.Controllers
 {
     public class AppController : Controller
     {
+        public readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
         public IActionResult Index()
         {
             return View();
@@ -27,6 +35,8 @@ namespace DotNetCorePluralsight.Controllers
             if (ModelState.IsValid)
             {
                 //send email
+                _mailService.SendMessage("arkarshwebaw@gmail.com",model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
+               ViewBag.UserMessage("Mail Sent");
             }
             else
             {
--- DotNetCorePluralsight/Startup.cs
+++ DotNetCorePluralsight/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetCorePluralsight.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +16,8 @@ namespace DotNetCorePluralsight
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IMailService, NullMailService>();
+            //support fore real mail service
             services.AddMvc();
         }
 
